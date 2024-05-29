e5f5724d7ed1e76f8bc2c5e42a6bb6830651d78e
Admin <admin@Admins-MacBook-Pro.local>
2024-05-29 18:45:46 +0530

    Vue js

---
+++ app.js
@@ -0,0 +1,8 @@
+Vue.createApp({
+    data()
+        {
+            return {
+                firstName: 'John'
+            }
+        }
+}).mount('#app')
--- day1.js
+++ day1.js
@@ -1,1 +1,11 @@
-console.log("hello")
+console.log("Day 1")
+var age = '24'
+let age1 = 24
+console.log(age1)
+console.log(age)
+var x = null
+var y = undefined
+console.log(x)
+console.log(y)
+let z = Symbol("symbol")
+console.log(z)
