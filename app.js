47cb1190f9f24d067530ce3e2e6665e42df7990b
Admin <admin@Admins-MacBook-Pro.local>
2024-05-30 01:47:04 +0530

    Vue project 1

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
-Vue.createApp({
+const vm = Vue.createApp({
     data()
         {
             return {
-                firstName: 'John'
+                firstName: 'John',
+                lastName: 'Doe',
+                url: 'https://google.com',
+                age: 10
+            }
+        },
+        methods: {
+            fullName() {
+                return `${this.firstName} ${this.lastName}`
+            },
+            increment(){
+                this.age++
+            },
+            decrement(){
+                this.age--
+            }
+        },
+        computed: {
+            fullNameComputed(){
+                console.log('This is a computed property')
+                return `${this.firstName} ${this.lastName}`
+            }
+        },
+        watch: {
+            age(newVal, oldVal) {
+                if( newVal < 10) {
+                    alert("age is less than 10")
+                }
+
             }
         }
 }).mount('#app')
+
+// without a proxy
+// setTimeout(() =>
+// {
+//     vm.firstName = "Raam"
+// }, 4000)
